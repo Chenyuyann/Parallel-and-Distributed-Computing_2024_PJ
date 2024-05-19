5b46d2196beab871bfc4ebdd8bdbac8af828d464
ChenYuyan <790597778@qq.com>
2024-05-19 15:54:55 +0800

    Modify .gitignore

--- qsortomp.cpp
+++ qsortomp.cpp
@@ -16,11 +16,11 @@ int Partition(int* data, int start, int end)
     return start;
 }
  
-void quickSort(int* data, int start, int end) //并行快排
+void quickSort(int* data, int start, int end) // 并行快排
 {
     if (start < end) {
         int pos = Partition(data, start, end);
-        #pragma omp parallel sections //设置并行区域
+        #pragma omp parallel sections // 设置并行区域
         {
             #pragma omp section
             quickSort(data, start, pos - 1);
@@ -32,20 +32,20 @@ void quickSort(int* data, int start, int end) //并行快排
  
 int main(int argc, char* argv[])
 {
-    int n = atoi(argv[2]), i; //线程数
-    int size = atoi(argv[1]); //数据大小
+    int n = atoi(argv[2]), i; // 线程数
+    int size = atoi(argv[1]); // 数据大小
     int* num = (int*)malloc(sizeof(int) * size);
  
     double starttime = omp_get_wtime();
     srand(time(NULL) + rand());
     for (i = 0; i < size; i++)
         num[i] = rand();
-    omp_set_num_threads(n); //设置线程数
+    omp_set_num_threads(n); // 设置线程数
     quickSort(num, 0, size - 1);
     double endtime = omp_get_wtime();
  
     // for (i = 0; i < 10 && i<size; i++)
     //     printf("%d ", num[i]);
-    printf("\n并行时间: %lfs\n", endtime - starttime);
+    printf("并行时间: %lfs\n", endtime - starttime);
     return 0;
 }
